242bb250459041ca874869987cb53ac5b9d15900
Deepak2004Bisht <deepakbisht2777@gmail.com>
2024-11-26 22:41:24 +0530

    Updated css file and jsx file

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,31 +2,33 @@ import React from 'react';
 import "../Styles/Container.css";
 export default function Container() {
   return (
-    <div className='container'>
+    <div className="banner">
       <div className='container-left'>
         <img src="../Images/logo.png" alt="" />
       </div>
 
-      <div className='content'>
-        <div className='text'>
-          <p>New Modern Fashion Stylist</p>
-          <h1>Exclusive Winter Collection 2025</h1>  
+      <div className="content">
+        <div className="text">
+          <p>New Modern Fashion Styles</p>
+          <h1>Exclusive Winter Collection 2022</h1>
+          <button className='btn'>Shop Now</button>
         </div>
-        <button className='btn'>Shop Now</button>
-    
+      </div>
 
-      <div class="circle">
-        <div class="arrow left">
-          <i class="fa-solid fa-arrow-left"></i>
+      <div className="circle">
+        <div className="arrow left">
+          <i className="fa-solid fa-arrow-left"></i>
         </div>
-        <div class="arrow right">
-          <i class="fa-solid fa-arrow-right"></i>
+        <div className="arrow right">
+          <i className="fa-solid fa-arrow-right"></i>
         </div>
       </div>
-      </div>
-      <div className='container-right'>
+      <div className="image container-right">
         <img src="../Images/logo(2).png" alt="" />
       </div>
     </div>
+
+
+    
   );
 }
