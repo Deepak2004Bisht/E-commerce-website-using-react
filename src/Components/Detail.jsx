a8021fe79d8ab8736671732df7eb35d39c6820cf
Deepak2004Bisht <deepakbisht2777@gmail.com>
2024-12-07 19:47:49 +0530

    Latest Blog Post

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -18,14 +18,14 @@ export default function Detail() {
 
         <div className="service-box">
             <i class="fa-solid fa-headphones"></i>
-            <h3>Free Shipping WorldWide</h3>
-            <p>Talk about your service</p>
+            <h3>Online Support</h3>
+            <p>24 hours a day, 7 day a week</p>
         </div>
 
         <div className="service-box">
-            <i class="fa-solid fa-truck-fast"></i>
-            <h3>Free Shipping WorldWide</h3>
-            <p>Talk about your service</p>
+            <i class="fa-solid fa-money-bill"></i>
+            <h3>Payment Return</h3>
+            <p>Pay with worldwide credit cards.</p>
         </div>
     </div>
   );
