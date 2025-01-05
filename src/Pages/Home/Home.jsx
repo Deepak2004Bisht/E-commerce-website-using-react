eae67b2b3670b8035e3750fc527a134f79cf0b1e
Deepak2004Bisht <deepakbisht2777@gmail.com>
2025-01-05 12:46:24 +0530

    Add Section1 component with OwlCarousel for homepage slider and update package dependencies

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Header from "../../Components/Header";
 import Footer from '../../Components/Footer';
+import Section1 from './Section1';
 
 export default function Home() {
   return (
     <div>
+
+      {/* Free shipping oder of navbar */}
+      <div className="promotion-box">
+        <div className="container-fluid">
+          <p className="promotion-text">
+            Free Shipping on all orders over $100!
+            <a className="link" href="#">
+              Shop Now
+            </a>
+          </p>
+          <span className="promotion-close">
+            <i className="ti-close" />
+          </span>
+        </div>
+      </div>
       <Header/>
-      about
+
+      <div className="page-container">
+        <Section1/>
+      </div>
+
       <Footer/>
     </div>
   );
